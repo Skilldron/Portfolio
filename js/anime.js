d1b5e80aea9060873bee2cd8dff7ce4bd92115fa
harold delory <harold_delory@ens.univ-artois.fr>
2021-01-13 14:14:27 +0100

    feat: v1.0 init

---
+++ js/anime.js
@@ -0,0 +1,24 @@
+// Animation d'apparition du menu réduit.
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
+
+// Fait apparaitre une barre noir à la nav barre une fois scroll
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+        $('.nav').addClass('affix');
+        console.log("OK");
+    } else {
+        $('.nav').removeClass('affix');
+    }
+});
+
+// Animation de disparition de l'alerte 
+$('.close-btn').click(function(){
+    $('.alerte').removeClass("show");
+    $('.alerte').addClass("hide");
+});
