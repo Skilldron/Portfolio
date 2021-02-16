26c1433b309bb1775a675aa1734450f8f3c671a5
harold delory <harold_delory@ens.univ-artois.fr>
2021-02-16 12:34:15 +0100

    Compression des images pour les allégés + fix tri des projets

--- js/anime.js
+++ js/anime.js
@@ -11,7 +11,6 @@ $('.navTrigger').click(function () {
 $(window).scroll(function() {
     if ($(document).scrollTop() > 50) {
         $('.nav').addClass('affix');
-        console.log("OK");
     } else {
         $('.nav').removeClass('affix');
     }
